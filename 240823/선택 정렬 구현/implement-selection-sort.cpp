963f2591f8d45128e999c1497a6441852f0444f2
Shim Hun <90015362+SHIMHUN@users.noreply.github.com>
2024-08-23 17:07:57 +0900

    Title: 선택 정렬 구현, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240823/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> nums;
+    int num;
+    for(int i=0; i<n; i++){
+        cin >> num;
+        nums.push_back(num);
+    }
+
+    for(int i=0; i<n; i++){
+        int min = i;
+        for(int j=i+1; j<n; j++){
+            if(nums[min] > nums[j]){
+                min = j;
+            }
+        }
+        int tmp = nums[i];
+        nums[i] = nums[min];
+        nums[min] = tmp;
+    }
+
+    for(int i=0; i<n; i++){
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
